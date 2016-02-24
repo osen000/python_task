358b2fc5b0a625eec936bcd90609cc9aca0e94e2
osen000 <osen000@mail.ru>
2016-02-24 20:53:14 +0300

    задание16

--- test/test_delete_group.py
+++ test/test_delete_group.py
@@ -3,16 +3,6 @@
 from model.group import Group
 from random import randrange
 
-# def test_delete_first_group(app):
-#     if app.group.count_group() == 0:
-#         app.group.create(Group(name="test"))
-#     old_groups = app.group.get_group_list()
-#     app.group.delete_first_group()
-#     assert len(old_groups) - 1 == app.group.count_group()
-#     new_groups = app.group.get_group_list()
-#     old_groups[0:1] = []
-#     assert old_groups == new_groups
-
 
 def test_delete_some_group(app):
     if app.group.count_group() == 0:
