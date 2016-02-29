50396f667ce24a4175503a225d6d068ad4fc6198
osen000 <osen000@mail.ru>
2016-02-29 18:31:48 +0300

    задание 19

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from model.contact import Contact
-import pytest
-# from data.add_contact import testdata
-from data.add_contact import constant as testdata
 
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_address(app, contact):
+
+def test_add_address(app, json_contacts):
+    contact = json_contacts
     old_contact = app.contact.get_contact_list()
     app.contact.create(contact)
     assert len(old_contact) + 1 == app.contact.count_contact()
